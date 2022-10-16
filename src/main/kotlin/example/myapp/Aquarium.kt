627ac0d1f5722fddd673907aaf13d8a8d1521b39
200103396 <91262551+200103396@users.noreply.github.com>
2022-10-17 02:33:47 +0600

    testing constr2

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -8,6 +8,8 @@ class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
     constructor(numberOfFish: Int) : this() {
         // 2,000 cm^3 per fish + extra room so water doesn't spill
         val tank = numberOfFish * 2000 * 1.1
+        // calculate the height needed
+        height = (tank / (length * width)).toInt()
     }
     init {
         println("aquarium initializing")
