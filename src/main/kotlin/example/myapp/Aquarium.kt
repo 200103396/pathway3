a12c924c3e17b83d11c1e890324d0afd46b7855d
200103396 <91262551+200103396@users.noreply.github.com>
2022-10-17 02:31:40 +0600

    volume

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -5,7 +5,13 @@ class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
     var length: Int = length
     var width: Int = width
     var height: Int = height
-
+    init {
+        println("aquarium initializing")
+    }
+    init {
+        // 1 liter = 1000 cm^3
+        println("Volume: ${width * length * height / 1000} liters")
+    }
     fun printSize() {
         println("Width: $width cm " +
                 "Length: $length cm " +
