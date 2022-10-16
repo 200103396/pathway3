2ed7e9df89daedea2f8a97a770b86be5ffcbbcbb
200103396 <91262551+200103396@users.noreply.github.com>
2022-10-17 02:43:53 +0600

    open value

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,31 +1,21 @@
 package example.myapp
 
-class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
-    // Dimensions in cm
-    var length: Int = length
-    var width: Int = width
-    var height: Int = height
-
-    var volume: Int
+open class Aquarium (
+    open var length: Int = 100, open var width: Int = 20, open var height: Int = 40) {
+    open var volume: Int
         get() = width * height * length / 1000
         set(value) {
             height = (value * 1000) / (width * length)
         }
-
-    constructor(numberOfFish: Int) : this() {
-        // 2,000 cm^3 per fish + extra room so water doesn't spill
-        val tank = numberOfFish * 2000 * 1.1
-        // calculate the height needed
-        height = (tank / (length * width)).toInt()
-    }
-    init {
-        println("aquarium initializing")
-    }
-
+    open val shape = "rectangle"
+    open var water: Double = 0.0
+        get() = volume * 0.9
     fun printSize() {
+        println(shape)
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
-        println("Volume: $volume liters")
+        // 1 l = 1000 cm^3
+        println("Volume: $volume liters Water: $water liters (${water / volume * 100.0}% full)")
     }
 }
