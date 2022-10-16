2ec5f92e91f6186d93a5c11dbb730a7c973b47f8
200103396 <91262551+200103396@users.noreply.github.com>
2022-10-17 02:38:46 +0600

    set Meth

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -5,8 +5,13 @@ class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
     var length: Int = length
     var width: Int = width
     var height: Int = height
-    val volume: Int
-        get() = width * height * length / 1000  // 1000 cm^3 = 1 liter
+
+    var volume: Int
+        get() = width * height * length / 1000
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+
     constructor(numberOfFish: Int) : this() {
         // 2,000 cm^3 per fish + extra room so water doesn't spill
         val tank = numberOfFish * 2000 * 1.1
