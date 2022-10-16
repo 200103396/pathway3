e4ab814f512d298f834b966dabae0c3cf574c441
200103396 <91262551+200103396@users.noreply.github.com>
2022-10-17 02:37:04 +0600

    printVolume

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -5,6 +5,8 @@ class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
     var length: Int = length
     var width: Int = width
     var height: Int = height
+    val volume: Int
+        get() = width * height * length / 1000  // 1000 cm^3 = 1 liter
     constructor(numberOfFish: Int) : this() {
         // 2,000 cm^3 per fish + extra room so water doesn't spill
         val tank = numberOfFish * 2000 * 1.1
@@ -14,13 +16,11 @@ class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
     init {
         println("aquarium initializing")
     }
-    init {
-        // 1 liter = 1000 cm^3
-        println("Volume: ${width * length * height / 1000} liters")
-    }
+
     fun printSize() {
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
+        println("Volume: $volume liters")
     }
 }
