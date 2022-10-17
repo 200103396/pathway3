e0e33390092ba9ed81f7d6884ed700a903d51f4d
200103396 <91262551+200103396@users.noreply.github.com>
2022-10-17 11:55:58 +0600

    add decor meth

---
+++ src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -0,0 +1,11 @@
+package example.myapp.decor
+
+data class Decoration(val rocks: String) {
+}
+fun makeDecorations() {
+    val decoration1 = Decoration("granite")
+    println(decoration1)
+}
+fun main(){
+    makeDecorations()
+}
